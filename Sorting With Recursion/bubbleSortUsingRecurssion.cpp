d820987124d8d2af1bc15e30cdffa65176c9f838
Yagyaraj Lodhi <101352591+yagyaraj234@users.noreply.github.com>
2023-03-19 00:31:39 +0530

    Update bubbleSortUsingRecurssion.cpp

--- Sorting With Recursion/bubbleSortUsingRecurssion.cpp
+++ Sorting With Recursion/bubbleSortUsingRecurssion.cpp
@@ -4,11 +4,11 @@ using namespace std;
 void bubbleSort(int *arr,int n){
 
     // Base Case 
-    if(n==0 ||n==0){
+    if(n==0 ||n==1){
         return ;
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n-1; i++)
     {
         if(arr[i]>arr[i+1]){
             swap(arr[i],arr[i+1]);
